a36b99cb001395b83d293b71ab25ce12cdfe4f45
zempo <zelenkosolomon@gmail.com>
2019-11-18 18:18:53 -0800

    update migrations

--- migrations/001.do.create_scripts.sql
+++ migrations/001.do.create_scripts.sql
@@ -5,7 +5,7 @@ author TEXT DEFAULT 'J. Silvers',
 subtitle TEXT DEFAULT 'An Original Screenplay',
 body TEXT DEFAULT '',
 actors TEXT [] DEFAULT ARRAY['John', 'Jane']::text[],
-tags TEXT [] DEFAULT ARRAY['Int', 'Ext', 'Description', 'Line-break']::text[],
+tags TEXT [] DEFAULT ARRAY['Int', 'Ext', 'Description', 'Angle On', 'Close-up', 'Continuation', 'Dissolve To', 'Fade In', 'Fade Out', 'Insert', 'Intercut', 'Montage', 'O.S.', 'POV', 'Series of Shots', 'Split Screen', 'Super', 'V.O.', 'Line-Break']::text[],
 date_created TIMESTAMP DEFAULT now() NOT NULL,
 date_updated TIMESTAMP 
 ); 
